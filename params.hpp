eab283adbd910aac582ada419867f40c028a3571
InnovationStudios101 <innovationstudios101@gmail.com>
2015-05-05 21:34:00 -0500

    Initial LSO repository setup.

---
+++ params.hpp
@@ -0,0 +1,12 @@
+// Enter your defaultparams here
+// Referenced from core/modules/modules.hpp
+
+/*
+	 class MyMissionParam
+	 { 
+		  title = Large Scale Operations v0.1;
+		  values[] = {};
+		  texts[] = {};
+		  default = 0;
+	 };
+*/	
